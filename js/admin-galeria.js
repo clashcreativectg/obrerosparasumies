24179d83e6a6259b1433f619ec2c65b68359fee1
clashcreativectg <clashcreative123@gmail.com>
2026-04-02 16:53:48 -0500

    Update admin-galeria.js

--- js/admin-galeria.js
+++ js/admin-galeria.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import {
   getFirestore,
   collection,
