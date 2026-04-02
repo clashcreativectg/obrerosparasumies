d3948c0c896811e66be723a4f81ad589db645032
clashcreativectg <clashcreative123@gmail.com>
2026-04-02 16:55:04 -0500

    Update galeria-firebase.js

--- js/galeria-firebase.js
+++ js/galeria-firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import {
   getFirestore,
   collection,
